06dbf0c3af8edf5e7ceaf39a4ae0993bf1a7896f
pierre0210 <conandoyle.0210@gmail.com>
2022-09-08 15:25:36 +0800

    edit project page

--- pages/project.tsx
+++ pages/project.tsx
@@ -10,33 +10,18 @@ const Project : NextPage = () => {
 			<NavBar/>
 			<div className={styles.Project}>
 				<div>
-					<h1>Logger Discord Bot</h1>
-					<h2>Features</h2>
-					<ul>
-						<li>Log Text Messages</li>
-						<li>Save deleted Attachments</li>
-						<li>Reminder command</li>
-					</ul>
-					<h2>Requirements</h2>
-					<ul>
-						<li>.Net 6</li>
-						<li>RedisDB</li>
-					</ul>
+					<h1>Sauce Discord Bot (TypeScript)</h1>
+					<Link href='https://github.com/pierre0210/Sauce-bot'>
+						<a>Github repo</a>
+					</Link>
+					<br></br>
+					<br></br>
+					<h1>Logger Discord Bot (C#)</h1>
 					<Link href='https://github.com/pierre0210/Logger-Bot'>
 						<a>Github repo</a>
 					</Link>
 					<br></br>
 					<br></br>
-					<Image
-						src="https://i.imgur.com/NNcdZc6.png"
-						alt="Picture of the logger bot"
-						width={508}
-						height={633}
-						layout="intrinsic"
-					/>
-				</div>
-				<div>
-					<h1>More projects coming soon...</h1>
 				</div>
 			</div>
 		</>
