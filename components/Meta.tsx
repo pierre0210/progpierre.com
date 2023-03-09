736943f1597e19712b667be22f818a8c174ca254
pierre0210 <gamelauncher0210@gmail.com>
2023-03-10 00:49:45 +0800

    fix: icon

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import favicon from '../public/favicon.ico';
 
 type metadata = {
 	title: string;
@@ -11,7 +12,7 @@ const Meta = ({ title, thumbnail, description, themeColor }: metadata) => {
 	return (
 		<Head>
 			<title>{title}</title>
-			<link rel="icon" href="/favicon.ico" />
+			<link rel="shortcut icon" href={favicon.src} />
 			<meta property="og:image" content={thumbnail} />
 			<meta name="description" content={description} />
 			<meta name="theme-color" content={themeColor} />
