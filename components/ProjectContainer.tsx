7bebc6f50f032836920ef0a3f1fdc482e6ce5fee
pierre0210 <gamelauncher0210@gmail.com>
2023-05-22 21:08:38 +0800

    fix: project page link hover effect

--- components/ProjectContainer.tsx
+++ components/ProjectContainer.tsx
@@ -13,7 +13,10 @@ const ProjectContainer = ({ title, url, languages }: param) => {
 			<div className="block p-1 w-2/3">
 				<h1 className="font-NotoSans font-bold text-3xl">{title}</h1>
 				<Link href={url}>
-					<a className="text-lg font-Roboto-Mono hover:underline hover:font-bold">Github</a>
+					<a className="inline-block text-lg font-Roboto-Mono group transition duration-300">
+						Github
+						<span className="flex max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-[#025b96]"></span>
+					</a>
 				</Link>
 			</div>
 			<div className="lg:m-0 w-1/3 md:flex md:px-3">
