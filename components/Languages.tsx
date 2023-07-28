d01a564c00d8075fc331d6d109eb8ca127b138b6
pierre0210 <gamelauncher0210@gmail.com>
2023-07-29 00:12:38 +0800

    feat: add layout type option

--- components/Languages.tsx
+++ components/Languages.tsx
@@ -2,22 +2,23 @@ import Image from "next/image";
 
 type param = {
 	languages: string[],
+	layoutType: "fixed" | "fill" | "intrinsic" | "responsive" | undefined,
 	size: number
 };
 
 // https://devicon.dev/
-const Languages = ({ languages, size }: param) => {
+const Languages = ({ languages, layoutType, size }: param) => {
 	return (
 		<>
 			{
 				languages.map((language) => (
-					<div key={language} className="p-1 justify-center md:w-40">
+					<div key={language} className="p-1 justify-center">
 						<Image
 							src={"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/" + language + "/" + language + "-original" +
 								(language === "go" ? "-wordmark" : "") + ".svg"}
 							width={size}
 							height={size}
-							layout="responsive"
+							layout={layoutType}
 							alt={language}
 						/>
 					</div>
